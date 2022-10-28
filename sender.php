6f807a8753297190677309439858ff35b47efd8b
Albert.ES <art.bertes@gmail.com>
2022-10-28 22:47:51 +0600

    fix

--- sender.php
+++ sender.php
@@ -3,23 +3,41 @@
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
 
-    require "PHPMailer/src/Exception.php";
     require "PHPMailer/src/PHPMailer.php";
+    require "PHPMailer/src/Exception.php";
+    
 
     $mail = new PHPMailer(true);
     $mail->CharSet = "UTF-8";
+    $mail->IsHTML(true);
 
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["text"];
 
-    $body = $name . ' ' . $email . ' ' . $message;
-    $theme = "заявка с формы]";
+    $email_template = "template_mail.html";
+    
+    $body = file_get_contents($email_template);
 
-    $mail->addAddress("art.bertes@gmail.com");
+	$body = str_replace('%name%', $name, $body);
+	$body = str_replace('%email%', $email, $body);
+	$body = str_replace('%message%', $message, $body);
 
-    $mail->Subject = $theme;
-    $mail->Body = $body;
+    $mail->addAddress("art.bertes@gmail.com");   // Здесь введите Email, куда отправлять
+	$mail->setFrom($email);
+    $mail->Subject = "[Заявка с формы]";
+    $mail->MsgHTML($body);
+    
+    if (!$mail->send()) {
+        $message = "Ошибка отправки";
+    } else {
+        $message = "Данные отправлены!";
+    }
+	
+	$response = ["message" => $message];
 
-    $mail->send();
+    header('Content-type: application/json');
+    echo json_encode($response);
+
+?>
 
